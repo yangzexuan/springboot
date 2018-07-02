291e7db6402108f1460af54e99644e6b692e0761
yangzexuan <yangzexuan@users.noreply.github.com>
2018-07-02 14:54:54 +0800

    springboot项目 热部署

---
+++ src/main/java/com/dome/springboot/DomeSpringBootApplication.java
@@ -0,0 +1,10 @@
+package com.dome.springboot;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class DomeSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DomeSpringBootApplication.class,args);
+    }
+}
